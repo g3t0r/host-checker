3b4ff5d6dcb4fe602a7f7896fd6f2a0e97fbda58
Jan Jakubowski <jan.jakubowski.ti@gmail.com>
2023-03-09 20:44:57 +0100

    converting 32 bit ip to string

--- src/ip-utils.c
+++ src/ip-utils.c
@@ -4,13 +4,46 @@
 #include "stdio.h"
 
 
-uint32_t ipv4fromString(const char *str)
+uint8_t * extractOctetsFromInt(uint32_t ipv4) {
+    uint8_t * octets = malloc(sizeof(uint8_t) * 4);
+    for(int i = 0; i < 4; i++) {
+        uint8_t octet = (ipv4 << (i * 8)) >> 24;
+        octets[i] = octet;
+
+    }
+    return octets;
+}
+
+char *ipv4ToString(uint32_t ipv4)
 {
+    char * strIp = calloc(sizeof(char), 16); // 16 - 4x3 + 3 dots + terminating null
+    uint8_t * octets = extractOctetsFromInt(ipv4);
+    int shift = 0;
+    for(int i = 0; i < 3; i++) {
+        shift += sprintf(strIp + shift, "%u.", octets[i]);
+    }
+    shift += sprintf(strIp + shift, "%u.", octets[3]);
+    strIp[shift] = '\0';
+    free(octets);
+    return strIp;
+}
+
+uint32_t convertOctetsToInt(uint8_t * octets) {
+    uint32_t combined = 0;
+    for(int i = 0; i < 3; i++) {
+        combined += octets[i];
+        combined <<= 8;
+    }
+    combined += octets[3];
+    return combined;
+}
+
+uint8_t * extractOctets(const char *str) {
     if(strlen(str) > 15) { // 4x3 numbers + 3 dots
         exit(1); 
     }
     short shift = 0;
-    uint8_t octets[4]; // left to right order
+    uint8_t * octets =  malloc(sizeof(uint8_t) * 4); // left to right order
     for(int oct = 1; oct <= 4; oct++) {
         int octInDec;
         int stringOctetSize;
@@ -34,16 +67,18 @@ uint32_t ipv4fromString(const char *str)
         octInDec =  atoi(strOct);
         free(strOct);
         shift = shiftUpdate;
-        printf("%d\n", octInDec);
         octets[oct - 1] = octInDec;
     }
 
-    return 0;
-    
+    return octets;
 }
 
-
-char *ipv4ToString(uint32_t ipv4)
+uint32_t ipv4fromString(const char *str)
 {
-    return 0;
+    uint8_t * octets = extractOctets(str);
+    uint32_t converted = convertOctetsToInt(octets);
+    free(octets);
+    return converted;
 }
+
+
--- src/main.c
+++ src/main.c
@@ -1,6 +1,15 @@
 #include "ip-utils.h"
+#include "stdio.h"
 
 int main() {
-    ipv4fromString("192.168.0.1");
+    uint32_t ip = ipv4fromString("192.168.0.1");
+    uint32_t mask = ipv4fromString("255.255.255.0");
+
+
+    printf("IP: %u\nMASK: %u\n", ip, mask);
+
+    char * asString = ipv4ToString(ip);
+    printf("IP as string: %s\n", asString);
+    
     return 0;
 }
